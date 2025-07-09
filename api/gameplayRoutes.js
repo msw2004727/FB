a4ec92d4bf1abd60011dcd970e715aece062d4c6
msw2004727 <msw741121@gmail.com>
2025-07-09 14:02:35 +0800

    Update gameplayRoutes.js

--- api/gameplayRoutes.js
+++ api/gameplayRoutes.js
@@ -7,9 +7,7 @@ const interactionRoutes = require('./interactionRoutes');
 const combatRoutes = require('./combatRoutes');
 
 // 將對應的請求分發給它們處理
-// 例如，當請求進來時，這個路由器會檢查 interactionRoutes 和 combatRoutes 是否有對應的處理方式
 router.use(interactionRoutes);
-router.use(combatRoutes);
+router.use(combatRoutes); // 【核心修改】新增這一行，讓主路由認識戰鬥路由
 
-// 這個總路由器現在只負責分發任務
 module.exports = router;
