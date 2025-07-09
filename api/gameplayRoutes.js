3df7b87998f0dd2a0e297af87e2b3b967bd66f60
msw2004727 <msw741121@gmail.com>
2025-07-09 14:06:33 +0800

    Update gameplayRoutes.js

--- api/gameplayRoutes.js
+++ api/gameplayRoutes.js
@@ -8,6 +8,6 @@ const combatRoutes = require('./combatRoutes');
 
 // 將對應的請求分發給它們處理
 router.use(interactionRoutes);
-router.use(combatRoutes); // 【核心修改】新增這一行，讓主路由認識戰鬥路由
+router.use(combatRoutes);
 
 module.exports = router;
